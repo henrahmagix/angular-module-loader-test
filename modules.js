7b994a2890403cb2c9af21dd116bb3adb1b98e6d
Henry Blyth <blyth.henry@gmail.com>
2016-07-11 14:06:10 +0100

    Improve test so both are run at same time

--- modules.js
+++ modules.js
@@ -1,33 +1,55 @@
 (function (angular) {
 
-    var mode = window.location.hash.slice(1);
-
-    var create = function (name) {
-        if (mode === 'typical') {
-            angular.module(name, [])
-                .factory(name + 'Object1', function () {return {};});
-        } else {
-            angular.module(name + 'Factory1', []).factory(name + 'Object1', function () {return {};});
-            angular.module(name + 'Factory2', []).factory(name + 'Object2', function () {return {};});
-            angular.module(name + 'Factory3', []).factory(name + 'Object3', function () {return {};});
-            angular.module(name + 'Factory4', []).factory(name + 'Object4', function () {return {};});
-            angular.module(name + 'Factory5', []).factory(name + 'Object5', function () {return {};});
-            angular.module(name, [
-                name + 'Factory1',
-                name + 'Factory2',
-                name + 'Factory3',
-                name + 'Factory4',
-                name + 'Factory5'
-            ]);
-        }
+    var createTypical = function (name) {
+        angular.module(name, [])
+            .factory(name + 'Object1', function () {return {};});
     };
 
-    var all = [];
-    for (var i = 0; i < 100; i++) {
-        all[i] = i.toString();
-        create(all[i]);
-    }
+    var createModular = function (name) {
+        angular.module(name + 'Factory1', []).factory(name + 'Object1', function () {return {};});
+        angular.module(name + 'Factory2', []).factory(name + 'Object2', function () {return {};});
+        angular.module(name + 'Factory3', []).factory(name + 'Object3', function () {return {};});
+        angular.module(name + 'Factory4', []).factory(name + 'Object4', function () {return {};});
+        angular.module(name + 'Factory5', []).factory(name + 'Object5', function () {return {};});
+        angular.module(name, [
+            name + 'Factory1',
+            name + 'Factory2',
+            name + 'Factory3',
+            name + 'Factory4',
+            name + 'Factory5'
+        ]);
+    };
 
-    var app = angular.module('app', all);
+    var allTypical = [];
+    var allModular = [];
+
+    var LIMIT = 100;
+
+
+
+    console.time('Typical');
+
+    for (var i = 0; i < LIMIT; i++) {
+        allTypical[i] = 'typical' + i.toString();
+        createTypical(allTypical[i]);
+    }
+    angular.module('app-typical', allTypical);
+    angular.bootstrap(document.getElementById('app-typical'), ['app-typical']);
+
+    console.timeEnd('Typical');
+
+
+
+    console.time('Modular');
+
+    for (var i = 0; i < LIMIT; i++) {
+        allModular[i] = 'modular' + i.toString();
+        createModular(allModular[i]);
+    }
+    angular.module('app-modular', allModular);
+    angular.bootstrap(document.getElementById('app-modular'), ['app-modular']);
+
+    console.timeEnd('Modular');
+
 
 }(window.angular));
